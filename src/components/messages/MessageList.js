415142579700f226cc414accc0098c5f4a3b4cc1
Helen <helen@Helens-MacBook-Pro.local>
2018-09-25 15:20:19 -0500

    editUserSpecific

--- src/components/messages/MessageList.js
+++ src/components/messages/MessageList.js
@@ -6,9 +6,38 @@ import Datamanager from '../../modules/Datamanager';
 export default class MessageList extends Component {
 
     state = {
-        messages:[]
+        messages:[],
+        currentUser: '',
     }
+
+    editUserSpecific = function(message){
+        if(message.currentUser === this.state.currentUser){
+            return <div>
+                 <button type="button"
+                                className="btn btn-success"
+                            onClick={() =>  this.props.history.push(`/messages-edit/${message.id}`)}
+                            className="card-link">Edit</button>
+                                       
+                                        
+            </div>
+        }
+    }
+
+
+    
     componentDidMount() {
+
+        let currentUser = JSON.parse(sessionStorage.getItem("credentials"))
+
+        Datamanager.getAll('users').then((result) => {
+            let findCurrentUser = result.find(item => {
+                return currentUser.id === item.id
+            })
+            if (findCurrentUser) {
+               let userFirstName = findCurrentUser.firstName 
+               this.setState({currentUser: userFirstName})
+            }
+        })   
         
         Datamanager.getAll('messages', 'messages')
           .then(messages => {
@@ -31,11 +60,7 @@ export default class MessageList extends Component {
                                 <h4 className="message-username">{message.currentUser}</h4>
                                 <h6 className="message-Date">{Date()}</h6>
                                 </div>
-                                <button type="button"
-                                className="btn btn-success"
-                            onClick={() =>  this.props.history.push(`/messages-edit/${message.id}`)}
-                            className="card-link">Edit</button>
-                               
+                                {this.editUserSpecific(message)}
                             </div>
                         </div>
                     </div>
